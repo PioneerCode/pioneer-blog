71563a542d92b6566b00f6a16e18b3243c474262
Chad Ramos <jedi_obi_wan_@hotmail.com>
2016-11-11 13:32:02 -0600

    Abstracted layout/template and adjusted gulp build to facilitate new area sharing.

---
+++ src/Pioneer.Blog/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pioneer.Blog.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Admin Categories";
+            return View();
+        }
+    }
+}
---
+++ src/Pioneer.Blog/Areas/Admin/Controllers/PostController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pioneer.Blog.Areas.Admin.Controllers
+{
+    [Area("admin")]
+
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Admin Posts";
+            return View();
+        }
+    }
+}
