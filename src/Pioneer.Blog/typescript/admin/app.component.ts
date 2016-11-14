7e8e35579271c53a5ff0bbbeedf93f82e1c128fa
Chad Ramos <jedi_obi_wan_@hotmail.com>
2016-11-14 13:09:04 -0600

    Boilerplate-ed Angular 2 in admin section.  Abstracted Admin and Public Gulp builds.

---
+++ src/Pioneer.Blog/typescript/admin/app.component.ts
@@ -0,0 +1,6 @@
+﻿import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>Hello Angular!</h1>'
+})
+export class AppComponent { }
---
+++ src/Pioneer.Blog/typescript/public/app.ts
@@ -0,0 +1,18 @@
+﻿/// <reference path='../typings/tsd.d.ts' />
+$(() => {
+  $(document).foundation();
+  $('.title-bar').removeClass('hide');
+});
+
+var header = $('.nav-main');
+
+$(window).on('scroll', () => {
+  var top = header.offset().top;
+  if (top >= 100) {
+    header.addClass('nav-main-show');
+  } else {
+    header.removeClass('nav-main-show');
+  }
+});
+
+$(window).trigger('scroll');
