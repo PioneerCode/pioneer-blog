b982614206fb34e7834336d35b7d651f46ed91a8
Chad <jedi_obi_wan_@hotmail.com>
2016-11-29 14:00:52 -0600

    Added client-side validation

--- src/Pioneer.Blog/gulp-tasks/public.js
+++ src/Pioneer.Blog/gulp-tasks/public.js
@@ -47,6 +47,7 @@ function libs() {
     'bower_components/foundation-sites/dist/plugins/foundation.responsiveToggle.js',
     'bower_components/foundation-sites/dist/plugins/foundation.util.mediaQuery.js',
     'bower_components/foundation-sites/dist/plugins/foundation.equalizer.js',
+    'bower_components/foundation-sites/dist/plugins/foundation.abide.js',
     'bower_components/parallax.js/parallax.js',
     'scripts/syntaxhighlighter.js'
   ])
