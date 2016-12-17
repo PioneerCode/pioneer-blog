e37dcb3955b3f8babd96a5181f74fb3a5ddfe994
Chad <jedi_obi_wan_@hotmail.com>
2016-12-17 12:31:25 -0600

    Article View Model and controller

---
+++ src/Pioneer.Blog.Model/Views/ArticleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pioneer.Blog.Model.Views
+{
+    public class ArticleViewModel
+    {
+        public string ArticleSection { get; set; }
+        public string ArticlePublishedTime { get; set; }
+        public string ArticleModifiedTime { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ src/Pioneer.Blog/Controllers/Web/ArticleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pioneer.Blog.Controllers.Web
+{
+    public class ArticleController : Controller
+    {
+        // GET: Blog
+        public ActionResult VisualStudioShortcuts()
+        {
+            ViewBag.Description = "Visual Studio Shortcuts";
+            return View("~/Views/Article/VisualStudioShortcuts.cshtml");
+        }
+    }
+}
