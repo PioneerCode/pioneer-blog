3e418623057f3f18c08c5494df0593c69fbadcd2
Chad <jedi_obi_wan_@hotmail.com>
2017-01-01 13:56:52 -0600

    Card styles

--- src/Pioneer.Blog/gulp-tasks/public.js
+++ src/Pioneer.Blog/gulp-tasks/public.js
@@ -22,24 +22,25 @@ function clean() {
 }
 
 function styles() {
+  //.pipe(uncss({
+  //  html: [
+  //    'http://localhost:8000',
+  //    'http://localhost:8000/styles.html',
+  //    'http://localhost:8000/about',
+  //    'http://localhost:8000/contact',
+  //    'http://localhost:8000/blog',
+  //    'http://localhost:8000/account/register',
+  //    'http://localhost:8000/post/developing-a-net-core-site-in-windows-and-deploying-it-to-a-budget-linux-host',
+  //    'http://localhost:8000/post/asp-net-core-mvc-pagination-using-a-tag-helper',
+  //    'http://localhost:8000/article/visual-studio-shortcuts'
+  //  ]
+  //}))
   return gulp.src(['./dev/public/app.scss'])
     .pipe(sass({
       includePaths: sassPaths
     }).on('error', sass.logError))
     .pipe(cleanCss({ keepSpecialComments: 0 }))
-    .pipe(uncss({
-      html: [
-        'http://localhost:8000',
-        'http://localhost:8000/styles.html',
-        'http://localhost:8000/about',
-        'http://localhost:8000/contact',
-        'http://localhost:8000/blog',
-        'http://localhost:8000/account/register',
-        'http://localhost:8000/post/developing-a-net-core-site-in-windows-and-deploying-it-to-a-budget-linux-host',
-        'http://localhost:8000/post/asp-net-core-mvc-pagination-using-a-tag-helper',
-        'http://localhost:8000/article/visual-studio-shortcuts'
-      ]
-    }))
+
     .pipe(autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
