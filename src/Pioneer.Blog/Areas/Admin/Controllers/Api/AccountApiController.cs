a1be05d4c1de5b6eb0a8c6556f7a404ed03c3de1
Chad Ramos <jedi_obi_wan_@hotmail.com>
2017-03-21 16:16:55 -0500

    Create api registration endpoint

---
+++ src/Pioneer.Blog/Areas/Admin/Controllers/Api/AccountApiController.cs
@@ -0,0 +1,46 @@
+﻿#if (DEBUG)
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Pioneer.Blog.DAL.Entites;
+using Pioneer.Blog.Model;
+
+namespace Pioneer.Blog.Areas.Admin.Controllers.Api
+{
+    [Route("api/accounts")]
+    public class AccountApiController : Controller
+    {
+        private readonly UserManager<UserEntity> _userManager;
+        private readonly SignInManager<UserEntity> _signInManager;
+
+        public AccountApiController(UserManager<UserEntity> userManager, 
+            SignInManager<UserEntity> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AccountRegister model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.Values.SelectMany(v => v.Errors).Select(modelError => modelError.ErrorMessage).ToList());
+            }
+
+            var user = new UserEntity { UserName = model.Email, Email = model.Email };
+            var result =  await _userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors.Select(x => x.Description).ToList());
+            }
+
+            await _signInManager.SignInAsync(user, false);
+
+            return Ok();
+        }
+    }
+}
+#endif
