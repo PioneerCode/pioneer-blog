d281c794788b3ef9f3a4d75ecae1b11a3d32d067
Chad Ramos <jedi_obi_wan_@hotmail.com>
2017-07-07 14:07:25 -0500

    Remove article from sitemap

--- src/Pioneer.Blog.Service/SiteMapService.cs
+++ src/Pioneer.Blog.Service/SiteMapService.cs
@@ -65,8 +65,6 @@ namespace Pioneer.Blog.Service
                 root.Add(GetGenericElement(xmlns, string.Format(url, "category/" + category.Url)));
             }
 
-            root.Add(GetGenericElement(xmlns, string.Format(url, "article/visual-studio-shortcuts")));
-
             return root.ToString();
         }
 
