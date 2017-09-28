c22b835d7c6219e97ae62a4e600dc083dc21b984
chad-ramos <jedi_obi_wan_@hotmail.com>
2017-09-27 21:11:13 -0500

    update typescript dep

--- src/Pioneer.Blog/dev/admin/components/categories/app.module.ts
+++ src/Pioneer.Blog/dev/admin/components/categories/app.module.ts
@@ -1,4 +1,5 @@
-﻿import { NgModule } from '@angular/core';
+﻿/// <reference path="category.service.ts" />
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
