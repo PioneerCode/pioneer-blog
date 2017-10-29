910497afcba1a7beb820ce06077a741bbd90f3ed
chad-ramos <jedi_obi_wan_@hotmail.com>
2017-10-29 18:33:04 -0500

    Add routing to admin portal page

--- src/Pioneer.Blog/Areas/Admin/Controllers/Web/PortalController.cs
+++ src/Pioneer.Blog/Areas/Admin/Controllers/Web/PortalController.cs
@@ -2,11 +2,12 @@
 
 namespace Pioneer.Blog.Areas.Admin.Controllers.Web
 {
-    public class HomeController : Controller
+    [Area("Admin")]
+    public class PortalController : Controller
     {
         public IActionResult Index()
         {
-            ViewBag.Title = "Admin | Categories";
+            ViewBag.Title = "Admin | Portal";
             ViewBag.Selected = "categories";
             ViewBag.SystemJsImportPath = "app/components/categories/main.js";
             return View();
