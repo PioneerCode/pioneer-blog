0a0e0792397a7e28046d4c66484e268ef1a735fc
chad-ramos <jedi_obi_wan_@hotmail.com>
2017-10-29 15:23:12 -0500

    Add default page

---
+++ src/Pioneer.Blog/Areas/Admin/Controllers/Web/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pioneer.Blog.Areas.Admin.Controllers.Web
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Admin | Categories";
+            ViewBag.Selected = "categories";
+            ViewBag.SystemJsImportPath = "app/components/categories/main.js";
+            return View();
+        }
+    }
+}
