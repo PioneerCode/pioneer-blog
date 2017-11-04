107fbb86994631c405aaf1eb378ffc01c0007c94
chad-ramos <jedi_obi_wan_@hotmail.com>
2017-11-04 15:03:23 -0500

    Migrated app to new version

---
+++ src/Pioneer.Blog/Areas/Admin/admin-app/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoriesPageComponent } from './components/pages/categories/categories-page.component';
+import { TagsPageComponent } from './components/pages/tags/tags-page.component';
+import { PostsPageComponent } from './components/pages/posts/posts-page.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/tags', pathMatch: 'full' },
+  { path: 'categories', component: CategoriesPageComponent },
+  { path: 'tags', component: TagsPageComponent },
+  { path: 'posts', component: PostsPageComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/Pioneer.Blog/Areas/Admin/admin-app/src/app/app.module.ts
+++ src/Pioneer.Blog/Areas/Admin/admin-app/src/app/app.module.ts
@@ -1,14 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PostsPageComponent } from './components/pages/posts/posts-page.component';
+import { CategoriesPageComponent } from './components/pages/categories/categories-page.component';
+import { TagsPageComponent } from './components/pages/tags/tags-page.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { PagerComponent } from './components/pager/pager.component';
+import { FormsModule } from '@angular/forms';
+import { TruncatePipe } from './pipes/truncate.pipe';
+import { TagService } from './components/pages/tags/tag.service';
+import { CategoryService } from './components/pages/categories/category.service';
+import { PostService } from './components/pages/posts/post.service';
+import { TagRepository } from './repositories/tag.repository';
+import { CategoryRepository } from './repositories/category.repository';
+import { PostTagRepository } from './repositories/post-tag.repository';
+import { PostRepository } from './repositories/post.repository';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   imports: [
-    BrowserModule
+    HttpModule,
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule
   ],
   declarations: [
-    AppComponent
+    TruncatePipe,
+    AppComponent,
+    TagsPageComponent,
+    CategoriesPageComponent,
+    PostsPageComponent,
+    LoaderComponent,
+    PagerComponent
+  ],
+  providers: [
+    PostRepository,
+    PostTagRepository,
+    CategoryRepository,
+    TagRepository,
+    PostService,
+    CategoryService,
+    TagService
   ],
   bootstrap: [ AppComponent ]
 })
