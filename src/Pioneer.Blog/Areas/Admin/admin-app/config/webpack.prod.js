fd9e99346f684bc1ee161db0cd4763ea59479859
chad-ramos <jedi_obi_wan_@hotmail.com>
2017-11-11 12:40:41 -0600

    Adjust build for production

--- src/Pioneer.Blog/Areas/Admin/admin-app/config/webpack.prod.js
+++ src/Pioneer.Blog/Areas/Admin/admin-app/config/webpack.prod.js
@@ -12,8 +12,8 @@ module.exports = webpackMerge(commonConfig, {
   output: {
     path: helpers.root('../../../wwwroot/admin'),
     publicPath: '/',
-    filename: '[name].[hash].js',
-    chunkFilename: '[id].[hash].chunk.js'
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js'
   },
 
   plugins: [
@@ -23,7 +23,7 @@ module.exports = webpackMerge(commonConfig, {
         keep_fnames: true
       }
     }),
-    new ExtractTextPlugin('[name].[hash].css'),
+    new ExtractTextPlugin('[name].css'),
     new webpack.DefinePlugin({
       'process.env': {
         'ENV': JSON.stringify(ENV)
