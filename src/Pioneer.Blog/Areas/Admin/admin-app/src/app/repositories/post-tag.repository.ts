b7662bfd3831c2dd464551ae5338ccb500ef28cd
chad-ramos <jedi_obi_wan_@hotmail.com>
2017-11-12 10:47:37 -0600

    Cleared length strings

--- src/Pioneer.Blog/Areas/Admin/admin-app/src/app/repositories/post-tag.repository.ts
+++ src/Pioneer.Blog/Areas/Admin/admin-app/src/app/repositories/post-tag.repository.ts
@@ -1,9 +1,7 @@
 ﻿import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-
-import 'rxjs/add/operator/toPromise';
-
 import { Tag } from '../models/tag';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class PostTagRepository {
