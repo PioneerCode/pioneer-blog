8d9e3a2af1a88e9f02d4c9c6e0c519cec78ee6ac
chad-ramos <jedi_obi_wan_@hotmail.com>
2017-11-12 10:18:18 -0600

    Re-traget assets

--- src/Pioneer.Blog/Areas/Admin/admin-app/config/webpack.common.js
+++ src/Pioneer.Blog/Areas/Admin/admin-app/config/webpack.common.js
@@ -32,7 +32,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-        loader: 'file-loader?name=assets/[name].[hash].[ext]'
+        loader: 'file-loader?name=../assets/[name].[hash].[ext]'
       },
       {
         test: /\.css$/,
