903f5d3ebbca645e2934732836ac24286ab7010d
chad-ramos <jedi_obi_wan_@hotmail.com>
2017-11-19 12:15:59 -0600

    Register deps

--- src/Pioneer.Blog/Startup.cs
+++ src/Pioneer.Blog/Startup.cs
@@ -4,8 +4,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
 using Pioneer.Blog.Entity;
+using Pioneer.Blog.Model;
 using Pioneer.Blog.Repository;
+using Pioneer.Blog.Service;
 using Pioneer.Blog.Services;
 
 namespace Pioneer.Blog
@@ -29,16 +32,11 @@ namespace Pioneer.Blog
                 .AddEntityFrameworkStores<BlogDbContext>()
                 .AddDefaultTokenProviders();
 
-            services.AddMvc()
-                .AddRazorPagesOptions(options =>
-                {
-                    options.Conventions.AuthorizeFolder("/Account/Manage");
-                    options.Conventions.AuthorizePage("/Account/Logout");
-                });
+            RegisterDependices(services);
 
-            // Register no-op EmailSender used by account confirmation and password reset during development
-            // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=532713
-            services.AddSingleton<IEmailSender, EmailSender>();
+            services.Configure<AppConfiguration>(Configuration.GetSection("AppConfiguration"));
+
+            ConfigureMvc(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,6 +63,44 @@ namespace Pioneer.Blog
                     name: "default",
                     template: "{controller}/{action=Index}/{id?}");
             });
+        }
+
+        private static void RegisterDependices(IServiceCollection services)
+        {
+            //services.AddTransient<IPaginatedMetaService, PaginatedMetaService>();
+
+            // Repositories
+            services.AddTransient<IContactRepository, ContactRepository>();
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<ITagRepository, TagRepository>();
+            services.AddTransient<IPostRepository, PostRepository>();
+            services.AddTransient<IPostTagRepository, PostTagRepository>();
+
+            // Services
+            services.AddTransient<ICategoryService, CategoryService>();
+            services.AddTransient<IPostService, PostService>();
+            services.AddTransient<IPostTagService, PostTagService>();
+            services.AddTransient<ISearchService, SearchService>();
+            services.AddTransient<ICommunicationService, CommunicationService>();
+            services.AddTransient<ITagService, TagService>();
+            services.AddTransient<ISiteMapService, SiteMapService>();
+            services.AddTransient<ApplicationEnvironment>();
+            //services.AddTransient<HostingEnvironment>();
+
+
+            // Register no-op EmailSender used by account confirmation and password reset during development
+            // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=532713
+            services.AddSingleton<IEmailSender, EmailSender>();
+        }
+
+        private static void ConfigureMvc(IServiceCollection services)
+        {
+            services.AddMvc()
+                .AddRazorPagesOptions(options =>
+                {
+                    options.Conventions.AuthorizeFolder("/Account/Manage");
+                    options.Conventions.AuthorizePage("/Account/Logout");
+                });
         }
     }
 }
