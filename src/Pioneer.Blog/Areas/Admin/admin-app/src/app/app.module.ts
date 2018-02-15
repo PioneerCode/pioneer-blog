d5e719d1a717b0c6ae9b2c64e2c4b352a2d1c14c
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-02-15 13:10:29 -0600

    Move source

--- src/Pioneer.Blog/Areas/Admin/admin-app/src/app/app.module.ts
+++ src/Pioneer.Blog/Areas/Admin/admin-app/src/app/app.module.ts
@@ -1,18 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpModule } from '@angular/http';
+import { TruncatePipe } from './pipes/truncate.pipe';
+import { TagsPageComponent } from './components/pages/tags/tags-page.component';
+import { CategoriesPageComponent } from './components/pages/categories/categories-page.component';
+import { PostsPageComponent } from './components/pages/posts/posts-page.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { PagerComponent } from './components/pager/pager.component';
+import { TagService } from './components/pages/tags/tag.service';
+import { CategoryService } from './components/pages/categories/category.service';
+import { PostService } from './components/pages/posts/post.service';
+import { TagRepository } from './repositories/tag.repository';
+import { CategoryRepository } from './repositories/category.repository';
+import { PostTagRepository } from './repositories/post-tag.repository';
+import { PostRepository } from './repositories/post.repository';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TruncatePipe,
+    TagsPageComponent,
+    CategoriesPageComponent,
+    PostsPageComponent,
+    LoaderComponent,
+    PagerComponent
   ],
   imports: [
-    BrowserModule
+    HttpModule,
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    PostRepository,
+    PostTagRepository,
+    CategoryRepository,
+    TagRepository,
+    PostService,
+    CategoryService,
+    TagService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
