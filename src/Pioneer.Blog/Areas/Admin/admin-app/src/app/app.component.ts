966a6dfe3f0d6a47d4c12f16b55e6a1482093f78
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-02-15 13:47:29 -0600

    Add foundation

--- src/Pioneer.Blog/Areas/Admin/admin-app/src/app/app.component.ts
+++ src/Pioneer.Blog/Areas/Admin/admin-app/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pc-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
