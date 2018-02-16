71e0d4b89ef6bc5e4714f5ed09a42fa441dffb24
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-02-16 15:19:28 -0600

    Add logic to handle responsive navigation

--- src/Pioneer.Blog/app-client/app.ts
+++ src/Pioneer.Blog/app-client/app.ts
@@ -1,32 +1,49 @@
 $(() => {
-    var doc = $(document);
-    doc.foundation();
-  
     /**
      * FOUT fighter
      */
     $('.title-bar').removeClass('hide');
-  });
-  
-  /**
-   * Cache navigation
-   */
+});
+
+/**
+ * Cache navigation
+ */
 var header = $('.nav-main');
-  
-  /**
-   * Manage transparent/un-transparent navigation
-   */
-  $(window).on('scroll', () => {
-      var top = header.offset().top;
+
+/**
+ * Manage transparent/un-transparent navigation
+ */
+$(window).on('scroll', () => {
+    var top = header.offset().top;
     if (top >= 100) {
-      header.addClass('nav-main-show');
+        header.addClass('nav-main-show');
     } else {
-      header.removeClass('nav-main-show');
-    }
-  });
-  
-  /**
-   * Trigger scroll event to handle middle of page loads for navigation
-   */
-  $(window).trigger('scroll');
-  
+        header.removeClass('nav-main-show');
+    } 
+});
+
+/**
+ * Trigger scroll event to handle middle of page loads for navigation
+ */
+$(window).trigger('scroll');
+
+/**
+ * Handle click event
+ */
+var closed = true;
+var menu = $('.mobile-nav');
+$('#hamburger').click(() => {
+    //if (this.closed) {
+    //    closed = false;
+    //    menu.addClass('show-for-small-only');
+    //    menu.removeClass('hide');
+    //    return;
+    //}
+
+    menu.toggleClass('show-for-small-only');
+    menu.toggleClass('hide');
+
+    //menu.removeClass('show-for-small-only');
+    //menu.addClass('hide');
+    //this.closed = true;
+});
