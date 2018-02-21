4ccdc2c9ad8629aaefa334404a4d8db78e6f8f6f
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-02-21 10:15:19 -0600

    Create Rss Service

---
+++ src/Pioneer.Blog.Service/RssService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Xml.Linq;
+using Microsoft.Extensions.Options;
+using Pioneer.Blog.Model;
+
+namespace Pioneer.Blog.Service
+{
+    public interface IRssService
+    {
+        string GetFeed();
+    }
+
+    public class RssService : IRssService
+    {
+        private readonly IPostService _postService;
+        private readonly ITagService _tagService;
+        private readonly ICategoryService _categoryService;
+        private readonly IOptions<AppConfiguration> _config;
+
+        public RssService(IPostService postService,
+            ITagService tagService,
+            ICategoryService categoryService,
+            IOptions<AppConfiguration> config)
+        {
+            _postService = postService;
+            _tagService = tagService;
+            _categoryService = categoryService;
+            _config = config;
+        }
+
+
+        public string GetFeed()
+        {
+            var doc = CreateDocument();
+            var channel = CreateChannel();
+            doc.Root.Add(channel);
+
+            foreach (var post in _postService.GetAll(false, false))
+            {
+                channel.Add(CreateChannelItem(post));
+            }
+
+            return doc.ToString();
+        }
+
+        private XDocument CreateDocument()
+        {
+            var doc = new XDocument(new XElement("rss"));
+            doc.Root.Add(new XAttribute("version", "2.0"));
+            return doc;
+        }
+
+        private XElement CreateChannel()
+        {
+            var channel = new XElement("channel");
+            channel.Add(new XElement("title", _config.Value.SiteUrl));
+            channel.Add(new XElement("link", $"{_config.Value.SiteUrl}/rssfeed.xml"));
+            channel.Add(new XElement("description", "Chad Ramos at Pioneer Code, a Chicago-based software developer with a strong passion for .NET, C#, The Web, Open Source, Programming and more."));
+            channel.Add(new XElement("copyright", $"© {DateTime.Now.Year} Pioneer Code"));
+            return channel;
+        }
+
+        private XElement CreateChannelItem(Post post)
+        {
+            var itemElement = new XElement("item");
+            itemElement.Add(new XElement("title", post.Title));
+            itemElement.Add(new XElement("link", $"{_config.Value.SiteUrl}/post/{post.Url}"));
+            itemElement.Add(new XElement("description", post.Description));
+            itemElement.Add(new XElement("category", post.Category.Name));
+
+            foreach (var c in post.Tags)
+            {
+                itemElement.Add(new XElement("tag", c.Name));
+            }
+
+            if (post.PostedOn != DateTime.MinValue) itemElement.Add(new XElement("pubDate", post.PostedOn.ToString("r")));
+
+            return itemElement;
+        }
+    }
+}
