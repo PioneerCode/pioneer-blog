c34dc6813585f0c0374ffb9932b8ea884abebb55
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-02-22 17:52:14 -0600

    Add login form

--- src/Pioneer.Blog/app-admin/src/app/components/pages/login/login.component.ts
+++ src/Pioneer.Blog/app-admin/src/app/components/pages/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
+  selector: 'pc-app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
