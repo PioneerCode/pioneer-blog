8a7f3853a5ce05370ebb2406ef9623a248484220
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-02-22 12:13:17 -0600

    Add screen switch

--- src/Pioneer.Blog/app-admin/src/app/app.component.ts
+++ src/Pioneer.Blog/app-admin/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './services/authentication.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pc-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(public authService: AuthenticationService) {
+
+  }
 }
