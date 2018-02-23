ee7801ab8643af7db531423bb8f0d3a4e83fd0be
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-02-22 18:00:30 -0600

    Add moc user repo

--- src/Pioneer.Blog/app-admin/src/app/components/pages/login/login.component.ts
+++ src/Pioneer.Blog/app-admin/src/app/components/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthenticationService } from '../../../services/authentication.service';
+import { UserRepository, ILoginRequest } from '../../../repositories/user.repository';
 
 @Component({
   selector: 'pc-app-login',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginRequest = {} as ILoginRequest;
+  loading = false;
+  returnUrl: string;
 
-  constructor() { }
+  constructor(
+    private userRepository: UserRepository,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) { }
+
 
   ngOnInit() {
-  }
+    // reset login status
+    this.authenticationService.logout();
 
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/home';
+  }
 }
---
+++ src/Pioneer.Blog/app-admin/src/app/repositories/user.repository.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { environment } from '../../environments/environment';
+import { IToken } from '../models/user';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/of';
+
+const API_URL = environment.apiUrl;
+
+export interface ILoginRequest {
+  username: string;
+  password: string;
+}
+
+@Injectable()
+export class UserRepository {
+
+  constructor(private http: HttpClient) { }
+
+  login(loginModel: ILoginRequest): Observable<IToken> {
+    return Observable.of<{ token: 'a' }>();
+    // return this.http.post(`${API_URL}/api-token-auth/`, loginModel)
+    //   .map((resp: IToken) => {
+    //     return resp || {} as IToken;
+    //   })
+    //   .catch(this.handleError);
+  }
+
+  private handleError(error: Response | any) {
+    console.error('UserRepository::handleError', error);
+    return Observable.throw(error);
+  }
+}
