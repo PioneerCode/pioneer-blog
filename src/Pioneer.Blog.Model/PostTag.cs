05f35b6dc31a2e87a6393b13908af9274625bc6f
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-06-05 15:49:08 -0500

    Scaffold project

--- src/Pioneer.Blog.Model/PostTag.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pioneer.Blog.Model
-{
-    public class PostTag
-    {
-        public int PostTagId { get; set; }
-        public int TagId { get; set; }
-        public int PostId { get; set; }
-    }
-}
